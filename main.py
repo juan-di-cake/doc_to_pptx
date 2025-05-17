3957d4f02b821a0ae5a9233587750022b34383e7
DESKTOP-AT4A258\USER <y7830877@gmail.com>
2025-05-17 18:07:04 +0800

    1.修復bug 在於標題選取 加入手動輸入標題 2.產生版本1.0.1

--- main.py
+++ main.py
@@ -77,38 +77,60 @@ def add_formatted_slide(title, content, slide_number):
     p.alignment = PP_ALIGN.RIGHT
 
 # 讀 Word 檔案
-def parse_docx(path):
+def parse_docx(path, title_lists):
     doc = docx.Document(path)
     current_title = None
     slides = []
 
     for para in doc.paragraphs:
         text = para.text.strip()
-        print("原始文字：", text)
-        print("______________________-")
         if not text:
             continue
-        if text.startswith("一、") or text.startswith("二、") or text.startswith("三、") or \
-           text.startswith("四、") or text.startswith("五、") or text.startswith("六、"):
-            current_title = text
-            print("標題：", current_title)
-        elif text.endswith("之禱"):
-            current_title = text
-            print("標題：", current_title)
-        elif text.startswith("倫理一則："):
-            current_title = "倫理一則"
-            content = text.split("：", 1)[-1].strip()
-            slides.append((current_title, content))
+        if any(title in text for title in title_lists):
+            
+            if "倫理一則" in text:
+                current_title = "倫理一則"
+                content = text.split("：", 1)[-1].strip() if "：" in text else text
+                slides.append((current_title, content))
+            else:
+                current_title = text
         elif current_title:
             if not text.startswith("詩歌:"):
                 slides.append((current_title, text))
-                
-                print("內容：", text)           
-                print("-------------")
-            else:
-                print(" 詩歌：", text)
-
+            
     return slides
+
+
+# -------------------original code-------------------
+
+    # for para in doc.paragraphs:
+    #     text = para.text.strip()
+    #     print("原始文字：", text)
+    #     print("______________________-")
+    #     if not text:
+    #         continue
+    #     if text.startswith("一、") or text.startswith("二、") or text.startswith("三、") or \
+    #        text.startswith("四、") or text.startswith("五、") or text.startswith("六、"):
+    #         current_title = text
+    #         print("標題：", current_title)
+    #     elif text.endswith("之禱"):
+    #         current_title = text
+    #         print("標題：", current_title)
+    #     elif text.startswith("倫理一則："):
+    #         current_title = "倫理一則"
+    #         content = text.split("：", 1)[-1].strip()
+    #         slides.append((current_title, content))
+    #     elif current_title:
+    #         if not text.startswith("詩歌:"):
+    #             slides.append((current_title, text))
+                
+    #             print("內容：", text)           
+    #             print("-------------")
+    #         else:
+    #             print(" 詩歌：", text)
+
+    # return slides
+
 
 def extract_specific_text(text):
     """
@@ -130,7 +152,8 @@ def extract_specific_text(text):
     return matches
     
 # create the gui 
-def create_gui_application(pre_check_title_list):
+def create_gui_application(
+):
 
     root = tk.Tk()
     # 固定視窗大小
@@ -138,7 +161,7 @@ def create_gui_application(pre_check_title_list):
     root.resizable(False, False)
     # 標題和版本
     root.title("禱告會PPT製作工具")
-    version_label = tk.Label(root, text="Version: 1.0.0", fg="gray")
+    version_label = tk.Label(root, text="Version: 1.0.1", fg="gray")
     version_label.pack(side=tk.BOTTOM, pady=5)
     input_docx_path = tk.Entry(root, width=40)
     # 選取檔案按鈕
@@ -165,9 +188,11 @@ def create_gui_application(pre_check_title_list):
     title_list_panel.pack(pady=5)
 
     # 產生標題列按鈕 
+    execute_button = tk.Button(root, text="產生標題列表", command=lambda: get_spec_titles(input_docx_path,title_list_panel))
 
+    execute_button.pack(pady=10)    
     # 產生PPT按鈕
-    execute_button = tk.Button(root, text="產生PPT", command=lambda: generate_output_ppt(input_docx_path,output_pptx_path,pre_check_title_list,title_list_panel))
+    execute_button = tk.Button(root, text="產生PPT", command=lambda: generate_output_ppt(input_docx_path,output_pptx_path,title_list_panel))
     execute_button.pack(pady=10)
     
 
@@ -175,7 +200,7 @@ def create_gui_application(pre_check_title_list):
 
     root.mainloop()
 
-def generate_output_ppt(input_docx_path, output_pptx_path,pre_check_title_list, title_lists):
+def generate_output_ppt(input_docx_path, output_pptx_path, title_lists):
 
 
 
@@ -187,35 +212,79 @@ def generate_output_ppt(input_docx_path, output_pptx_path,pre_check_title_list, 
         tk.messagebox.showerror("Error", "請選取正確的文字檔")
         return
 
-    if not pre_check_title_list:
-        tk.messagebox.showerror("Error", "請先按下方的產生標題列按鈕")
-        return
-    elif title_lists.get() == "":
-        tk.messagebox.showerror("Error", "請輸入標題列")
-        return    
+    if title_lists.get() == "":
+        tk.messagebox.showerror("Error", "請輸入標題列,或按產生標題列表")
+        return  
+    
     output_filename= output_pptx_path.get()
     if not output_filename:
         tk.messagebox.showerror("Error", "請輸入輸出的PPT名稱.")
         return
     
-    slides_data = parse_docx(docx_path)
+    title_lists = title_lists.get().split(",")
+    slides_data = parse_docx(docx_path, title_lists)
     for i, (title, content) in enumerate(slides_data, 1):
         add_formatted_slide(title, content, i)
+    
+    
     ppt.save(output_filename)
     tk.messagebox.showinfo("Success", f"產生成功~路徑在{output_filename}!")
+
     
 
-def get_spec_titles(slides_data):
+def get_spec_titles(input_docx_path, title_lists):
     """
         從 slides_data 中找出所有標題，回傳標題列表（去重、保留順序）
     """
+    docx_path =input_docx_path.get()
+    if not docx_path:
+        tk.messagebox.showerror("Error", "請選取文字檔")
+        return
+    elif not docx_path.endswith(".docx"):
+        tk.messagebox.showerror("Error", "請選取正確的文字檔")
+        return
+    
+    doc = docx.Document(docx_path)
+    
+    title_list = []
+
+    for para in doc.paragraphs:
+        text = para.text.strip()
+        
+        if not text:
+            continue
+        if text.startswith("一、") or text.startswith("二、") or text.startswith("三、") or \
+           text.startswith("四、") or text.startswith("五、") or text.startswith("六、"):
+            clean_title = re.sub(r"詩歌[:：].*$", "", text).strip()
+            title_list.append(clean_title)
+        elif text.endswith("之禱"):
+            clean_title = re.sub(r"詩歌[:：].*$", "", text).strip()
+            title_list.append(clean_title)
+        elif text.startswith("倫理一則：") or text.startswith("倫理一則"):
+            title_list.append("倫理一則")
+
+    
+    # 去重並保留順序
+    unique_titles = []
     seen = set()
-    titles = []
-    for title, _ in slides_data:
-        if title not in seen:
-            seen.add(title)
-            titles.append(title)
-    return titles
+    for t in title_list:
+        if t not in seen:
+            unique_titles.append(t)
+            seen.add(t)
+    title_lists.delete(0, tk.END)
+    title_lists.insert(0, ",".join(unique_titles))
+
+    # 將標題列表顯示在 GUI 上
+    title_list_str = "\n".join(unique_titles)
+             
+
+    
+
+
+    
+    
+    
+    
 
 def select_docx_file(input_docx_path):
     file_path = filedialog.askopenfilename(filetypes=[("Word Documents", "*.docx")])
@@ -227,7 +296,7 @@ def select_docx_file(input_docx_path):
 
 
 if __name__ == "__main__":
-    pre_check_title_list = False
+    
 
-    create_gui_application(pre_check_title_list)
+    create_gui_application()
     
